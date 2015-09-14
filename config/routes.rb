ece660da90cd37cfefe489f2786571a7c502a1e2
ytadavarthi3 <yasasvit@gatech.edu>
2015-09-14 17:29:04 -0400

    added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+
   resources :contacts
   resources :jobs
   resources :notes
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'boards#index'
+  root 'homes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
