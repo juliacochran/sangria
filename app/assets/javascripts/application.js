c9daeb9e2c520defe63a6e2b1e936f4ce96a1f0e
bradleyoesch <bradley.oesch@gmail.com>
2015-09-29 14:49:50 -0400

    added modal functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,16 @@
 //= require_tree .
 //= require materialize-sprockets
 
-$(".dropdown-button").dropdown();
+$(document).ready(function() {
+  $(".dropdown-button").dropdown();
+  $(".board-modal-trigger").leanModal();
+
+  $(".title-board").hover(function() {
+    var $this = $(this);
+    $this.find("span").show();
+  }, function() {
+    var $this = $(this);
+    $this.find("span").hide();
+  });
+});
+
