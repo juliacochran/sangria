d6b6e01815ad5f1bff8728b8b4d6475114c626d6
ytadavarthi3 <yasasvit@gatech.edu>
2015-09-29 22:14:56 -0400

    added modal for new app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@
 $(document).ready(function() {
   $(".dropdown-button").dropdown();
   $(".board-modal-trigger").leanModal();
-
+  $(".modal-trigger").leanModal();
   $(".title-board").hover(function() {
     var $this = $(this);
     $this.find("span").show();
