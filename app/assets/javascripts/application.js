d0f8392aa0d0256ae9fc5913974b2330526a527d
ytadavarthi3 <yasasvit@gatech.edu>
2015-09-29 23:04:00 -0400

    modal for new application works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,21 +5,22 @@
 //= require materialize-sprockets
 
 $(document).ready(function() {
-  $(".dropdown-button").dropdown();
-  $(".board-modal-trigger").leanModal();
-  $(".modal-trigger").leanModal();
-  $(".title-board").hover(function() {
-    var $this = $(this);
-    $this.find("span").show();
-  }, function() {
-    var $this = $(this);
-    $this.find("span").hide();
-  $(".board-change-modal-trigger").leanModal({
-    in_duration: 200,
-    out_duration: 200
-  });
-  $(".board-new-modal-trigger").leanModal({
-    in_duration: 200,
-    out_duration: 200
-  });
+	$(".dropdown-button").dropdown();
+	$(".board-modal-trigger").leanModal();
+	$(".modal-trigger").leanModal();
+	$(".title-board").hover(function() {
+		var $this = $(this);
+		$this.find("span").show();
+	}, function() {
+		var $this = $(this);
+		$this.find("span").hide();
+		$(".board-change-modal-trigger").leanModal({
+			in_duration: 200,
+			out_duration: 200
+		});
+		$(".board-new-modal-trigger").leanModal({
+			in_duration: 200,
+			out_duration: 200
+		});
+	});
 });
