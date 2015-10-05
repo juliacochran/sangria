4192d27186cb3a7ca7f43d7eacf0ce272482d843
Julia Cochran <juliacochran7@gmail.com>
2015-10-05 15:19:25 -0400

    fixed deleting objects

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,9 @@
 class Application < ActiveRecord::Base
-  belongs_to :board, dependent: :destroy
+  belongs_to :board
   validates_presence_of :board
   belongs_to :company
   #validates_presence_of :company
   has_one :job
-  has_many :interactions
+  has_many :interactions, dependent: :destroy
 
 end
