0caf0b873a506fb2471d09b90dacf9d8e27c21e3
Julia Cochran <juliacochran7@gmail.com>
2015-10-05 15:22:41 -0400

    added user id to companies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928200911) do
+ActiveRecord::Schema.define(version: 20151005192218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150928200911) do
     t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
