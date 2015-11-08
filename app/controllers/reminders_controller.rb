8dd78b4b0b7138934d9fb7c3c5870902f022e5dd
Julia Cochran <juliacochran7@gmail.com>
2015-11-08 18:46:11 -0500

    fixing category for emails and going through all users

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,13 +8,13 @@ class RemindersController < ApplicationController
 	def self.send_followup_reminders
 		# For all users we have
 		@allUsers = User.all
-		@allUsers.each do |@user|
+		@allUsers.each do |user|
 			# get interactions for that user, maybe later breakdown by category
-			@interactions = @user.interactions
+			@interactions = user.interactions
 			@interactions.each do |event|
 				today = Date.today
 				if today - event.date <= 2
-					NotifyMailer.followup_reminder(@user, event).deliver
+					NotifyMailer.followup_reminder(user, event).deliver
 				end
 
 			end
