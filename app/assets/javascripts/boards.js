3162bb6a7e7b7a9533a496cb595b90073088685c
uenowataru <wataruueno@gatech.edu>
2015-11-10 14:39:35 -0500

    added drag

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -684,3 +684,35 @@ $("#extruder").buildMbExtruder({
   slideTimer:300
 });
 
+
+$(function() {
+    var oldList, newList, item;
+    $('.sortable').sortable({
+        start: function(event, ui) {
+            item = ui.item;
+            newList = oldList = ui.item.parent().parent();
+        },
+        stop: function(event, ui) {          
+            //alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
+        },
+        change: function(event, ui) {  
+            if(ui.sender) newList = ui.placeholder.parent().parent();
+        },
+        connectWith: ".sortable"
+    }).disableSelection();
+});
+
+
+
+// $(document).ready(function(){  	
+//     $( "#sortable" ).sortable({
+//       revert: true
+//     });
+//     $( "#draggable" ).draggable({
+//       connectToSortable: "#sortable",
+//       helper: "clone",
+//       revert: "invalid"
+//     });
+//     $( "ul, li" ).disableSelection();
+//   });
+
