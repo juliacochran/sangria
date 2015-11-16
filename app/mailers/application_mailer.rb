13338de0fdda27591bf66841d4bdc705eea9c1ac
Julia Cochran <juliacochran7@gmail.com>
2015-11-16 16:04:48 -0500

    trying to fix link for follow-up in emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "sangrianotify@gmail.com"
+  ActionMailer::Base.default_url_options[:host] = "sangriaapp.tk"
+
   layout 'mailer'
 end
