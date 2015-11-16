1c2a0a13a0778e3ba8541764e6ccccb8566797b0
uenowataru <wataruueno@gatech.edu>
2015-11-16 16:48:13 -0500

    draggable

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -4,14 +4,29 @@ $(function() {
     $('.sortable').sortable({
         start: function(event, ui) {
             item = ui.item;
-            newList = oldList = ui.item.parent().parent();
+	        //console.log(newList);
         },
         stop: function(event, ui) {          
             //alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
         },
-        change: function(event, ui) {  
+        change: function(event, ui) {
             if(ui.sender) newList = ui.placeholder.parent().parent();
+
+        },
+        update: function(event, ui){
+    	    var num_stage = ui.item.parent().data('stage_num');
+    	    var application_id = ui.item.data('application_id');
+
+    	    console.log(num_stage);
+    	    console.log(application_id);
+
+    	    //TODO: update the stage
         },
         connectWith: ".sortable"
+
+        ,placeholder: "ui-state-highlight"
+
+    	//#,items: "li:not(:last-child)"
+
     }).disableSelection();
 });
